864d8e9b7a5b834413123f99b97719d0e3429c39
Michael Hansen <mihansen@microsoft.com>
2021-02-19 15:31:10 -0800

    Switched to HTTPS

--- scripts/update-index-html.sh
+++ scripts/update-index-html.sh
@@ -7,7 +7,7 @@ SCRIPTPATH=`dirname $SCRIPT`
 cat > $SCRIPTPATH/../index.html<< EOF
 <html>
   <head>
-    <meta http-equiv="refresh" content="0;url=http://ismrmrd.github.io/${folder}" />
+    <meta http-equiv="refresh" content="0;url=https://ismrmrd.github.io/${folder}" />
     <title></title>
   </head>
  <body></body>
