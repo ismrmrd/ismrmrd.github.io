61bc208e386ba57445719fffe8c78fd50620f529
Tobias Wuerfl <tobias.wuerfl@siemens-healthineers.com>
2021-06-10 14:47:03 +0200

    Created the documentation for version 1.4.1

---
+++ apidocs/1.4.1/search/all_6.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['get_5fdata_5ftype',['get_data_type',['../group__cxxapi.html#gaf67d53c80d44b2f283c67e70b24aa186',1,'ISMRMRD']]],
+  ['getdataptr',['getDataPtr',['../class_i_s_m_r_m_r_d_1_1_acquisition.html#a70d7d35e41a8ddfc6600bb8702a5198c',1,'ISMRMRD::Acquisition']]],
+  ['getdatasize',['getDataSize',['../class_i_s_m_r_m_r_d_1_1_image.html#aec8e190e240cf091f69a752c764bba37',1,'ISMRMRD::Image']]],
+  ['getnumberofdataelements',['getNumberOfDataElements',['../class_i_s_m_r_m_r_d_1_1_image.html#a064b30e8bccfa427f9e68f084f38adfe',1,'ISMRMRD::Image']]],
+  ['gettrajptr',['getTrajPtr',['../class_i_s_m_r_m_r_d_1_1_acquisition.html#a4850648ee7d6a98caa1fc91cfaa36451',1,'ISMRMRD::Acquisition']]]
+];
